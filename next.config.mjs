1c0451af48248b6ccb0622ed84249ba332b16d70
abhinandan-verma <abhinandanverma551@gmail.com>
2024-05-15 15:19:11 +0530

    refactor: Add 'example.com' to the list of allowed image domains

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         domains: [
              'static.vecteezy.com','www.studentresearchfoundation.org', 'cdn.sanity.io', 'skilledup.tech', 'uxwing.com',
-            'ik.imagekit.io', 'images.unsplash.com', 'skilledup.s3.ap-south-1.amazonaws.com',
+            'ik.imagekit.io', 'images.unsplash.com', 'skilledup.s3.ap-south-1.amazonaws.com','example.com',
             'cdn1.iconfinder.com', 'cdn-icons-png.flaticon.com', 'cdn.iconscout.com', 'm.economictimes.com',
             'upload.wikimedia.org','press.aboutamazon.in','etimg.etb2bimg.com','encrypted-tbn0.gstatic.com', 'encrypted-tbn0.gstatic.com'
         ],
