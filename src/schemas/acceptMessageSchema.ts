a770da23e886d85a8dc88a727d6dedfdb6bf95ff
abhinandan-verma <abhinandanverma551@gmail.com>
2024-05-17 17:31:42 +0530

    refactor: added important  files and update npm dependencies

--- src/schemas/acceptMessageSchema.ts
+++
@@ -1,5 +0,0 @@
-import * as z from "zod"
-
-export const isAcceptingMessageSchema = z.object({
-    isAcceptingMessages: z.boolean()
-})
--- src/schemas/signInSchema.ts
+++
@@ -1,6 +0,0 @@
-import * as z from "zod"
-
-export const signInSchema = z.object({
-    password: z.string().min(8).max(30),
-    email: z.string().email(),
-})
